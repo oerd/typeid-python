76aecfc926abd7a99ba0d0870440d0febc340a90
Oerd Cukalla <rosaccu@gmail.com>
2025-11-18 13:54:56 +0100

    Fix: replace single quotes with double quotes in prefix validation logic

--- typeid/validation.py
+++ typeid/validation.py
@@ -8,14 +8,14 @@ def validate_prefix(prefix: str) -> None:
     if prefix is None:
         raise PrefixValidationException("No prefix provided.")
 
-    if prefix[0] == '_' or prefix[-1] == '_':
+    if prefix[0] == "_" or prefix[-1] == "_":
         raise PrefixValidationException("Prefix cannot start or end with '_'.")
 
     if len(prefix) > 63:
         raise PrefixValidationException("Prefix cannot be longer than 63 characters.")
 
     for char in prefix:
-        if not ('a' <= char <= 'z' or char == '_'):
+        if not ("a" <= char <= "z" or char == "_"):
             raise PrefixValidationException("Prefix can only contain ASCII letters 'a-z' or '_'.")
 
 
